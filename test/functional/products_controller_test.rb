46dd3082823b448ccee2bcb5e7f5f3a33c7773fa
Luis <luistobon.mx@gmail.com>
2012-07-16 15:09:51 -0500

    Nueva línea en blanco
    Es una excelente idea!

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -7,6 +7,7 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 #---
+
 # Excerpted from "Agile Web Development with Rails, 4rd Ed.",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material, 
