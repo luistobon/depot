748828149452ab29c07ec82d081c41f7ad5d4315
Luis <luistobon.mx@gmail.com>
2012-07-16 14:56:27 -0500

    Segundo Commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, :price, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(jpg|png|gif)$}i,
+    :message => "La imagen debe ser jpg, png o gif."
+  }
 end
